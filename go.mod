1119558f89e9d1cfbc60f6a48d5c0f2f4d57de70
bjkang <kaq6822@naver.com>
2024-06-08 22:33:36 +0900

    feat: Hello grpc

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module grpc-go-server
 
 go 1.20
 
-require github.com/GRPCPractice/proto v0.0.0-20240608082503-82074b5c15ec // indirect
+require (
+	github.com/GRPCPractice/proto v0.0.0-20240608082503-82074b5c15ec
+	google.golang.org/grpc v1.64.0
+)
+
+require (
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/GRPCPractice/proto/proto/helloworld"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type server struct {
+	helloworld.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+	fmt.Println("Hello Request: ", in.GetName())
+
+	return &helloworld.HelloReply{Message: "Hello, World!"}, nil
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		fmt.Printf("failed to listen: %v", err)
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	helloworld.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		fmt.Printf("failed to serve: %v", err)
+	}
 }
